580a08cc8a2a7a29b92481f71e124792a18cfb33
szolla <35842149+szolla@users.noreply.github.com>
2019-02-06 10:16:54 -0600

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,8 @@
+variable "AaddsAdminUserUpn" {default = ""}
+variable "TenantId" {default = ""}
+variable "AzureSubscriptionId" {default = ""}
+variable "ManagedDomainName" {default = ""}
+variable "ResourceGroupName" {default = ""}
+variable "VnetName" {default = ""}
+variable "AzureLocation" {default = ""}
+variable "pw" {default = ""}
